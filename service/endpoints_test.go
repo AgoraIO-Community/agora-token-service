9a09c9d2458410e37b8b3eed438e2855ac56a834
Hermes (agora) <hermes.frangoudis@agora.io>
2024-06-12 17:52:12 -0400

    removed GET endpoint tests

--- service/endpoints_test.go
+++ service/endpoints_test.go
@@ -11,6 +11,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type UrlCodePair struct {
+	url  string
+	code int
+	body []byte
+}
+
 func TestGetTokenValidAndInvalid(t *testing.T) {
 
 	tests := []UrlCodePair{
@@ -41,103 +47,6 @@ func TestGetTokenValidAndInvalid(t *testing.T) {
 	}
 }
 
-func TestRtcValidAndInvalid(t *testing.T) {
-
-	tests := []UrlCodePair{
-		{"/rtc/fsda/publisher/uid/0/?expiry=600", http.StatusOK, nil},
-		{"/rtc/fsda/publisher/uid//?expiry=600", http.StatusOK, nil},
-		{"/rtc/fsda/publisher/uid/test/?expiry=600", http.StatusBadRequest, nil},
-		{"/rtc/fsda/publisher/uid/0/?expiry=failing", http.StatusBadRequest, nil},
-	}
-	for _, httpTest := range tests {
-		testApi, err := http.NewRequest(http.MethodGet, httpTest.url, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp := httptest.NewRecorder()
-		// Call the endpoint
-		testService.Server.Handler.ServeHTTP(resp, testApi)
-		assert.Equal(t, httpTest.code, resp.Code, resp.Body)
-	}
-}
-
-func TestRtmValidAndInvalid(t *testing.T) {
-
-	// Create a new gin engine for testing
-	reqValid, err := http.NewRequest(http.MethodGet, "/rtm/username/?expiry=600", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	reqInvalid, err := http.NewRequest(http.MethodGet, "/rtm/0/?expiry=600", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Create a response recorder to inspect the response
-	resp := httptest.NewRecorder()
-
-	// Call the endpoint
-	testService.Server.Handler.ServeHTTP(resp, reqValid)
-
-	assert.Equal(t, http.StatusOK, resp.Code)
-
-	resp = httptest.NewRecorder()
-
-	// Call the endpoint
-	testService.Server.Handler.ServeHTTP(resp, reqInvalid)
-
-	assert.Equal(t, http.StatusBadRequest, resp.Code)
-}
-
-type UrlCodePair struct {
-	url  string
-	code int
-	body []byte
-}
-
-func TestChatValidAndInvalid(t *testing.T) {
-
-	tests := []UrlCodePair{
-		{"/chat/app/", http.StatusOK, nil},
-		{"/chat/account/username/", http.StatusOK, nil},
-		{"/chat/account/", http.StatusNotFound, nil},
-		{"/chat/invalid/", http.StatusNotFound, nil},
-		{"/chat/account/username/?expiry=600", http.StatusOK, nil},
-		{"/chat/account/username/?expiry=fail", http.StatusBadRequest, nil},
-	}
-	for _, httpTest := range tests {
-		testApi, err := http.NewRequest(http.MethodGet, httpTest.url, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp := httptest.NewRecorder()
-		// Call the endpoint
-		testService.Server.Handler.ServeHTTP(resp, testApi)
-		assert.Equal(t, httpTest.code, resp.Code, resp.Body)
-	}
-}
-
-func TestRteValidAndInvalid(t *testing.T) {
-
-	tests := []UrlCodePair{
-		{"/rte/channelName/publisher/uid/0/rtmid/?expiry=600", http.StatusOK, nil},
-		{"/rte/channelName/publisher/uid/2345/?expiry=600", http.StatusOK, nil},
-		{"/rte/channelName/subscriber/uid/0/rtmid/?expiry=600", http.StatusOK, nil},
-		{"/rte/channelName/subscriber/uid/2345/?expiry=600", http.StatusOK, nil},
-		{"/rte/channelName/publisher/uid/0/?expiry=600", http.StatusBadRequest, nil},
-		{"/rte/channelName/publisher/uid/2345/?expiry=failing", http.StatusBadRequest, nil},
-	}
-	for _, httpTest := range tests {
-		testApi, err := http.NewRequest(http.MethodGet, httpTest.url, nil)
-		if err != nil {
-			t.Fatal(err)
-		}
-		resp := httptest.NewRecorder()
-		// Call the endpoint
-		testService.Server.Handler.ServeHTTP(resp, testApi)
-		assert.Equal(t, httpTest.code, resp.Code, resp.Body)
-	}
-}
-
 func TestPingPong(t *testing.T) {
 	testApi, err := http.NewRequest(http.MethodGet, "/ping", nil)
 	if err != nil {
@@ -158,43 +67,4 @@ func TestPingPong(t *testing.T) {
 	}
 
 	assert.Equal(t, "pong", response["message"])
-}
-
-func TestTokentypes(t *testing.T) {
-
-	// Create a new gin engine for testing
-	reqValidUid, err := http.NewRequest(http.MethodGet, "/rtc/fsda/publisher/uid/0/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Create a new gin engine for testing
-	reqValidUserAcc, err := http.NewRequest(http.MethodGet, "/rtc/fsda/publisher/userAccount/0/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	reqInvalid, err := http.NewRequest(http.MethodGet, "/rtc/fsda/publisher/nonsense/0/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// Create a response recorder to inspect the response
-	resp := httptest.NewRecorder()
-
-	// Call the endpoint
-	testService.Server.Handler.ServeHTTP(resp, reqValidUid)
-
-	assert.Equal(t, http.StatusOK, resp.Code)
-
-	resp = httptest.NewRecorder()
-
-	// Call the endpoint
-	testService.Server.Handler.ServeHTTP(resp, reqValidUserAcc)
-
-	assert.Equal(t, http.StatusOK, resp.Code)
-
-	resp = httptest.NewRecorder()
-
-	// Call the endpoint
-	testService.Server.Handler.ServeHTTP(resp, reqInvalid)
-
-	assert.Equal(t, http.StatusBadRequest, resp.Code)
 }
